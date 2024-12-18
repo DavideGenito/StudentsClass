beb0c0592d595c957a7ed2e6f247a270ef0ba217
Davide Genito <davidegenito2008@gmail.com>
2024-12-18 12:58:50 +0100

    primo

---
+++ PascalStudent.cs
@@ -0,0 +1,109 @@
+﻿namespace MarksAndStudent
+{
+    public enum Result
+    {
+        ADMITTED,
+        NOTADMITTED,
+        SUSPENDED
+    }
+    public class PascalStudent
+    {
+        private int[] _marks;
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+
+        public PascalStudent(bool isBiennium, string name, string surname)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentNullException("name");
+            if (string.IsNullOrWhiteSpace(surname)) throw new ArgumentNullException("surname");
+
+            if (isBiennium)
+                _marks = new int[13];
+            else
+                _marks = new int[10];
+
+            Name = name;
+            Surname = surname;
+        }
+
+        public void addMark(int mark)
+        {
+            if (mark <= 0 || mark > 10) throw new ArgumentOutOfRangeException("Mark not valid");
+
+            bool insert = false;
+            int count = 0;
+
+            while (!insert)
+            {
+                if (_marks[count] == 0)
+                {
+                    _marks[count] = mark;
+                    insert = true;
+                }
+                count++;
+            }
+        }
+
+        public double average()
+        {
+            int sum = 0;
+            int count = 0;
+            foreach (int mark in _marks)
+            {
+                if (mark == 0)
+                    count++;
+                else
+                    sum += mark;
+            }
+            if (count == _marks.Length)
+                return 0;
+            else
+                return (double)sum / (_marks.Length - count);
+        }
+
+        public int numberOfInsufficiencies()
+        {
+            int count = 0;
+            foreach (int mark in _marks)
+            {
+                if (mark < 6 && mark != 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public int pointToSufficiency()
+        {
+            int sum = 0;
+            foreach (int mark in _marks)
+            {
+                if (mark < 6 && mark != 0)
+                {
+                    sum += (6 - mark);
+                }
+            }
+            return sum;
+        }
+
+        public Result scrutiny()
+        {
+            // point in insufficiencies < 4 suspended, like if you have a 4 and a 5 you are suspended
+            // point in insufficiencies > 3 not admitted, so if you have a 3 and a 5 you are not admitted
+            if (pointToSufficiency() <= 3)
+                return Result.SUSPENDED;
+            else if (pointToSufficiency() > 3)
+                return Result.NOTADMITTED;
+            else
+                return Result.ADMITTED;
+
+        }
+
+        public int markOfSubject(int subject)
+        {
+            if (subject < 1 || subject > _marks.Length) throw new ArgumentOutOfRangeException("Subject not valid");
+            return _marks[subject-1];
+        }
+    }
+}
---
+++ Students.cs
@@ -0,0 +1,173 @@
+﻿using MarksAndStudent;
+
+namespace StudentsClass
+{
+    public class Students
+    {
+        private PascalStudent[] _students;
+
+        public Students(int numStudents)
+        {
+            _students = new PascalStudent[numStudents];
+        }
+
+        private int _numResult(Result result)
+        {
+            int count = 0;
+            foreach (PascalStudent student in _students)
+            {
+                if (student.scrutiny() == result) count++;
+            }
+            return count;
+        }
+
+        private PascalStudent[] _resultStudent(Result result)
+        {
+            PascalStudent[] positions = new PascalStudent[_numResult(result)];
+
+            int count = 0;
+
+            for (int i = 0; i < positions.Length; i++)
+            {
+                if (_students[i].scrutiny() == result)
+                {
+                    positions[count] = _students[i];
+                    count++;
+                }
+            }
+
+            return positions;
+        }
+
+        public int numberNotAdmitted()
+        {
+            return _numResult(Result.NOTADMITTED);
+        }
+
+        public PascalStudent[] studentsNotAdmitted()
+        {
+            return _resultStudent(Result.NOTADMITTED);
+        }
+
+        public int numberAdmitted()
+        {
+            return _numResult(Result.ADMITTED);
+        }
+
+        public PascalStudent[] studentsAdmitted()
+        {
+            return _resultStudent(Result.ADMITTED);
+        }
+
+        public int numberSuspended()
+        {
+            return _numResult(Result.SUSPENDED);
+        }
+
+        public PascalStudent[] studentsSuspended()
+        {
+            return _resultStudent(Result.SUSPENDED);
+        }
+
+        public double allStudentsAverage()
+        {
+            double sum = 0;
+            int count = 0;
+            foreach (PascalStudent student in _students)
+            {
+                if (student.average() == 0)
+                    count++;
+                else
+                    sum += student.average();
+            }
+
+            if (count == _students.Length)
+                return 0;
+            else
+                return sum / (_students.Length - count);
+        }
+
+        public PascalStudent lowestAverageStudent()
+        {
+            PascalStudent result = _students[0];
+            bool first = true;
+            foreach (PascalStudent student in _students)
+            {
+                if (!first)
+                {
+                    if (student.average() < result.average())
+                    {
+                        result = student;
+                    }
+                    else if (student.average() == result.average())
+                    {
+                        if(string.Compare(student.Surname, result.Surname) < 0)
+                        {
+                            result = student;
+                        }
+                        else if(string.Compare(student.Surname, result.Surname) == 0)
+                        {
+                            if (string.Compare(student.Name, result.Name) <= 0) result = student;
+                        }
+                    }
+                }
+                else
+                {
+                    first = false;
+                }
+            }
+            return result;
+        }
+
+        public PascalStudent highestAverageStudent()
+        {
+            PascalStudent result = _students[0];
+            bool first = true;
+            foreach (PascalStudent student in _students)
+            {
+                if (!first)
+                {
+                    if (student.average() > result.average())
+                    {
+                        result = student;
+                    }
+                    else if (student.average() == result.average())
+                    {
+                        if (string.Compare(student.Surname, result.Surname) > 0)
+                        {
+                            result = student;
+                        }
+                        else if (string.Compare(student.Surname, result.Surname) == 0)
+                        {
+                            if (string.Compare(student.Name, result.Name) >= 0) result = student;
+                        }
+                    }
+                }
+                else
+                {
+                    first = false;
+                }
+            }
+            return result;
+        }
+
+        public PascalStudent bestMark(int subject)
+        {
+            if (subject < 1 || subject > 13) throw new ArgumentOutOfRangeException("Subject not valid");
+            PascalStudent result = new PascalStudent(true, "d", "d");
+            bool first = true;
+
+            foreach (PascalStudent student in _students)
+            {
+                if (first)
+                {
+                    first = false;
+                }
+                else
+                {
+                    if(result.markOfSubject(subject) < student.markOfSubject(subject)) result = student;
+                }
+            }
+        }
+    }
+}
